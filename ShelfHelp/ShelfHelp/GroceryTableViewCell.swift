5d429e38336b159ca4c154e3af10e10b5793eb5e
HumzaS <humzamsiddiqui@gmail.com>
2016-02-17 21:15:52 -0500

    Added mealTableViewCell class and GroceryTableViewCell class

---
+++ ShelfHelp/ShelfHelp/GroceryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroceryTableViewCell.swift
+//  ShelfHelp
+//
+//  Created by Humza Siddiqui on 2/17/16.
+//  Copyright © 2016 Humza Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class GroceryTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var ingredientLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ShelfHelp/ShelfHelp/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  ShelfHelp
+//
+//  Created by Humza Siddiqui on 2/17/16.
+//  Copyright © 2016 Humza Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var mealLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
