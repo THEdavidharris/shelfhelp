88147811dcf68da8cae2432b5d19cf07905bf6b9
HumzaS <humzamsiddiqui@gmail.com>
2016-02-17 20:20:45 -0500

    added a search bar

---
+++ ShelfHelp/ShelfHelp/SearchMealsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchMealsViewController.swift
+//  ShelfHelp
+//
+//  Created by Humza Siddiqui on 2/17/16.
+//  Copyright © 2016 Humza Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class SearchMealsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShelfHelp/ShelfHelp/searchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  searchViewController.swift
+//  ShelfHelp
+//
+//  Created by Humza Siddiqui on 2/17/16.
+//  Copyright © 2016 Humza Siddiqui. All rights reserved.
+//
+
+import Foundation
