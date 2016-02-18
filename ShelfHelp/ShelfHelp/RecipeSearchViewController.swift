cb7e91a191bb9bb846e1f1fa1e5b22527accdd50
HumzaS <humzamsiddiqui@gmail.com>
2016-02-17 20:43:20 -0500

    Added RecipeSearch controller, and made search button remove keyboard

---
+++ ShelfHelp/ShelfHelp/RecipeSearchViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RecipeSearchViewController.swift
+//  ShelfHelp
+//
+//  Created by Humza Siddiqui on 2/17/16.
+//  Copyright © 2016 Humza Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class RecipeSearchViewController: UIViewController, UISearchBarDelegate {
+
+    // MARK: Properties
+    @IBOutlet weak var RecipeSearch: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        RecipeSearch.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UISearchBarDelegate
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        RecipeSearch.resignFirstResponder()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
